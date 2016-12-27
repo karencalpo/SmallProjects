f4e3697ef9ce88bf4747a9796dbdc7e11dae2be7
Karen Calpo <minnkc@gmail.com>
2016-12-27 09:06:08 -0800

    Made minor changes to index.html and script.js

--- catalina/js/script.js
+++ catalina/js/script.js
@@ -1,6 +1,13 @@
 function login_form() {
     if(document.getElementById("login_button").innerHTML !== "Logout") {
         document.getElementById("login_form").style.display = 'block';
+    } else {
+        document.getElementById("login_form").style.display = 'block';
+        document.getElementById("username").value = "";
+        document.getElementById("password").value = "";
+        document.getElementById("login_button").innerHTML = "Login";
+        document.getElementById("welcome_text").style.display = 'none';
+        document.getElementById("user").innerHTML = "";
     }
 }
 
