9c46b8467590985a8feca37611dc63bc46a864a6
Karen Calpo <minnkc@gmail.com>
2017-01-11 19:22:06 -0800

    Updated files in Catalina folder.

--- catalina/js/script.js
+++ catalina/js/script.js
@@ -7,21 +7,26 @@ function login_form() {
         document.getElementById("password").value = "";
         document.getElementById("login_button").innerHTML = "Login";
         document.getElementById("welcome_text").style.display = 'none';
-        document.getElementById("user").innerHTML = "";
     }
 }
 
 function login() {
     
     var xhr = new XMLHttpRequest();
+   
     
-    xhr.open("GET", "http://projects.draconicscales.com/catalina/ajax_info/ajax_file.txt", true);
+    xhr.open("GET", "http://projects.draconicscales.com/catalina/ajax_info/welcome.json", true);
+    xhr.setRequestHeader("Content-Type", "application/json");	
+
     xhr.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
+	     var data = this.responseText;
+            var jsonResponse = JSON.parse(data);
             var user = document.getElementById("username").value;
-            document.getElementById("user").innerHTML = user;
+
             document.getElementById("login_button").innerHTML = "Logout";
             document.getElementById("welcome_text").style.display = 'inline-block';
+	     document.getElementById("welcome_text").innerHTML = jsonResponse.message + user;
             document.getElementById("login_form").style.display = 'none';
         }
     };
