fc3515dc9d0cc4ac8dfea97f4dbadb035fd350cb
Karen Calpo <minnkc@gmail.com>
2017-03-28 22:12:06 -0700

    Changes to Loan Calculator to include RESTful elements.

--- Loan_Calculator/scripts/script.js
+++ Loan_Calculator/scripts/script.js
@@ -1,22 +1,20 @@
 var loan_amt_id = document.getElementById('loan_amt');
-var annual_interest_id = document.getElementById("annual_interest");
+var annual_interest_id = document.getElementById('annual_interest');
 var repayment_period_id = document.getElementById('repayment_period');
 var zipcode_id = document.getElementById('zipcode');
-var phone_id = document.getElementById('phone');
 
 loan_amt_id.addEventListener("blur", validate_dollar);
 annual_interest_id.addEventListener("blur", validate_interest);
 repayment_period_id.addEventListener("blur", validate_years);
 zipcode_id.addEventListener("blur", validate_zipcode);
-phone_id.addEventListener("blur", validate_phone);
 
 function validate_dollar() {
     var loan_amt = loan_amt_id.value;
     var dollar_exp = /^\$?\d+(\.(\d{2}))?$/;
     
-    if(!dollar_exp.test(loan_amt) && loan_amt_id.value !== undefined) {
-        alert("Please enter a valid dollar amount without a dollar sign for the loan (no dollar signs or commas).");
-        loan_amt_id.value = undefined;
+    if(!dollar_exp.test(loan_amt) && loan_amt_id.value !== "") {
+        alert("Please enter a valid dollar amount without a dollar sign for the loan (No dollar signs or commas).");
+        loan_amt_id.value = "";
     } 
 }
 
@@ -24,17 +22,17 @@ function validate_interest() {
     var annual_interest = annual_interest_id.value;
     var interest_exp = /^100$|^\d{0,2}(\.\d{1,2})? *%?$/;
     
-    if(!interest_exp.test(annual_interest) && annual_interest_id.value !== undefined) {
+    if(!interest_exp.test(annual_interest) && annual_interest_id.value !== "") {
         alert("Please enter a valid percentage for the interest (values between 1-100 without a % sign up to two decimal places).");
-        annual_interest_id.value = undefined;
+        annual_interest_id.value = "";
     } 
 }
 
 function validate_years() {
     var repayment_period = repayment_period_id.value;
-    if(isNaN(parseFloat(repayment_period)) && repayment_period_id.value !== undefined) {
+    if(isNaN(parseFloat(repayment_period)) && repayment_period_id.value !== "") {
         alert("Please a valid value for the number of years.");
-        repayment_period_id.value = undefined;
+        repayment_period_id.value = "";
     } 
 }
 
@@ -42,19 +40,9 @@ function validate_zipcode() {
     var zipcode = zipcode_id.value;
     var zipcode_exp = /^\d{5}$/;
     
-    if(!zipcode_exp.test(zipcode) && zipcode_id.value !== undefined) {
+    if(!zipcode_exp.test(zipcode) && zipcode_id.value !== "") {
         alert("Please enter a valid zipcode.");
-        zipcode_id.value = undefined;
-    } 
-}
-
-function validate_phone() {
-    var phone = phone_id.value;
-    var phone_exp = /[0-9]/;
-    
-    if(!phone_exp.test(phone) && phone_id.value !== undefined) {
-        alert("Please enter a valid phone number.");
-        phone_id.value = undefined;
+        zipcode_id.value = "";
     } 
 }
 
@@ -79,12 +67,40 @@ function Calculate() {
     var total_payment = monthly_payment * (12*repay_period);
     var total_interest = total_payment - loan;
 
-    if(loan_amt_id.value === undefined || annual_interest.value === undefined || repayment_period_id.value === undefined || zipcode_id.value === undefined) {
+    if(loan_amt_id.value === "" || annual_interest.value === "" || repayment_period_id.value === "" || zipcode_id.value === "") {
         alert("Please fill in values for Loan Amount, Annual Interest, Repayment Period, and Zip Code.");
     } else {
         document.getElementById('monthly_payment').value = monthly_payment.toFixed(2);
         document.getElementById('total_payment').value = total_payment.toFixed(2);
         document.getElementById('total_interest').value = total_interest.toFixed(2);
+        getLenders(loan, interest, repay_period, zip_code);
     }
 }
 
+function getLenders(amount, apr, years, zipcode) {
+    
+    
+    var xhr = new XMLHttpRequest();
+    var data = xhr.responseText;
+    var jsonResponse = JSON.parse(data);
+    
+    xhr.open("GET", "http://www.insurancesearch.com/api/lenders?loan_amt=" + amount + "&annual_interest=" + apr + "&repayment_period=" + years + "&zipcode=" + zipcode + '"', true);
+    xhr.setRequestHeader("Content-Type", "text/xml");
+    
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          for(key in jsonResponse) {
+              newlink = document.createElement('a');
+              newlink.innerHTML = "'" + key + "'";
+              newlink.setAttribute('title', "'" + key + "'");
+              newlink.setAttribute('href', "'" + jsonResponse[key] + "'");
+              document.getElementById("list").appendChild(newlink);
+          }
+
+          document.getElementById("lenders").style.display = 'block';
+        }
+    };
+    
+    xhr.send();
+    
+}
